29671e64b9c86fb29a225972a466eceb394b45d5
David Timm <dtimm@pivotal.io>
2020-01-28 11:21:36 -0700

    Read APP_SELECTOR from env
    - remove vendor directory

--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module example.com/metric-scraper
 go 1.13
 
 require (
-	github.com/Azure/go-autorest v11.1.2+incompatible // indirect
-	github.com/imdario/mergo v0.3.8 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
 	k8s.io/apimachinery v0.17.2
--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"os"
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
+	"k8s.io/metrics/pkg/apis/metrics/v1beta1"
 	"k8s.io/metrics/pkg/client/clientset/versioned"
 )
 
 func main() {
 	apiServer := os.Getenv("APISERVER")
 	token := os.Getenv("TOKEN")
+	appSelector := os.Getenv("APP_SELECTOR")
 	// caCertPath := os.Getenv("CACERT_PATH")
 	namespace := "default"
 	if value, ok := os.LookupEnv("NAMESPACE"); ok {
@@ -29,16 +33,30 @@ func main() {
 	}
 
 	metricsClient, _ := versioned.NewForConfig(restConfig)
-	// TODO: Use LabelSelector to select a particular pod
-	podMetrics, _ := metricsClient.MetricsV1beta1().PodMetricses(namespace).List(v1.ListOptions{})
+	var timeout int64 = 5
+	podMetrics, _ := metricsClient.MetricsV1beta1().PodMetricses(namespace).List(v1.ListOptions{
+		LabelSelector:       "app=" + appSelector,
+		TimeoutSeconds:      &timeout,
+	})
 
+	err := printMetrics(os.Stdout, podMetrics)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func printMetrics(w io.Writer, podMetrics *v1beta1.PodMetricsList) error {
+	b := bytes.NewBuffer([]byte(""))
 	for _, podMetric := range podMetrics.Items {
+		b.WriteString(fmt.Sprintf("Metrics for pod: %s\n", podMetric.Name))
 		for _, container := range podMetric.Containers {
-			fmt.Println("Metrics for container: ", container.Name)
+			b.WriteString(fmt.Sprintf("\tcontainer: %s\n", container.Name))
 			containerCPU := container.Usage.Cpu()
 			containerMemory := container.Usage.Memory()
-			fmt.Println("cpu: ", containerCPU)
-			fmt.Println("memory: ", containerMemory)
+			b.WriteString(fmt.Sprintf("\tcpu: %v\n", containerCPU))
+			b.WriteString(fmt.Sprintf("\tmemory: %v\n", containerMemory))
 		}
 	}
+	_, err := b.WriteTo(w)
+	return err
 }
