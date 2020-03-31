a8076774d5741eae31e64f9e1e818398bb1ade50
David Timm <dtimm@pivotal.io>
2020-03-31 18:14:55 +0000

    Added histogram metric for latency
    - added scrape annotations to metric-proxy pod
    
    [#171988813]
    
    Signed-off-by: Kaitlin Barrer <kbarrer@pivotal.io>

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"time"
 
 	"code.cloudfoundry.org/go-envstruct"
 	"code.cloudfoundry.org/log-cache/pkg/rpc/logcache_v1"
+	"code.cloudfoundry.org/metric-proxy/pkg/metrics"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
+
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/metrics/pkg/apis/metrics/v1beta1"
 	"k8s.io/metrics/pkg/client/clientset/versioned"
-
-	"code.cloudfoundry.org/metric-proxy/pkg/metrics"
 )
+
+var requestDurations = prometheus.NewHistogram(prometheus.HistogramOpts{
+	Name:    "request_duration_seconds",
+	Help:    "gPRC request duration distribution",
+	Buckets: prometheus.ExponentialBuckets(0.005, 2, 12),
+})
+
+func init() {
+	prometheus.MustRegister(requestDurations)
+}
 
 func main() {
 	cfg, err := LoadConfig()
@@ -36,7 +52,12 @@ func main() {
 		AddEmptyDiskEnvelope: true,
 	}
 
-	s := grpc.NewServer(grpc.Creds(cfg.TLS.Credentials("metric-proxy")))
+	startMetricsEndpoint()
+
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(requestTimer),
+		grpc.Creds(cfg.TLS.Credentials("metric-proxy")),
+	)
 	logcache_v1.RegisterEgressServer(s, c)
 
 	lis, err := net.Listen("tcp", cfg.Addr)
@@ -44,6 +65,22 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	panic(s.Serve(lis))
+}
+
+func requestTimer(ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler) (interface{}, error) {
+
+	start := time.Now()
+
+	h, err := handler(ctx, req)
+
+	d := time.Now().Sub(start)
+
+	requestDurations.Observe(d.Seconds())
+
+	return h, err
 }
 
 func createMetricsFetcher(cfg *Config) (metrics.Fetcher, error) {
@@ -63,4 +100,18 @@ func createMetricsFetcher(cfg *Config) (metrics.Fetcher, error) {
 			TimeoutSeconds: &cfg.QueryTimeout,
 		})
 	}, nil
+}
+
+func startMetricsEndpoint() {
+	lis, err := net.Listen("tcp", "localhost:9090")
+	if err != nil {
+		log.Printf("unable to start monitor endpoint: %s", err)
+	}
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	log.Printf("starting monitor endpoint on http://%s/metrics\n", lis.Addr().String())
+	go func() {
+		err = http.Serve(lis, mux)
+		log.Printf("error starting the monitor server: %s", err)
+	}()
 }
