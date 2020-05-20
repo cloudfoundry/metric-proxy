9b177dc398a6336eb0d5b6178a2f0dc6bbc93b56
Caitlyn Yu <cyu@pivotal.io>
2020-05-20 20:30:11 +0000

    Annotate metric_proxy image
    [#172921741]
    
    Signed-off-by: Louie Brann <lbrann@pivotal.io>

---
+++ scripts/build-images.sh
@@ -0,0 +1,22 @@
+DOCKER_ORG=${DOCKER_ORG:-oratos}
+REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
+DEPLAB=${DEPLAB:-true}
+
+function buildAndReplaceImage {
+    image=$1
+    srcFolder=$2
+    dockerfile=$3
+    yttValuesRef=$4
+
+    docker build -t $DOCKER_ORG/$image:latest $srcFolder -f $srcFolder/$dockerfile
+    if [ -n "$DEPLAB" ]; then
+        deplab --image $DOCKER_ORG/$image:latest --git ${REPO_DIR} --output-tar /tmp/$image --tag $DOCKER_ORG/$image:latest
+        docker load -i /tmp/$image
+    fi
+    docker push $DOCKER_ORG/$image:latest
+
+    imageRef="$(docker image inspect $DOCKER_ORG/$image:latest --format '{{index .Id}}')"
+    sed -i'' -e "s| $yttValuesRef:.*| $yttValuesRef: \"$DOCKER_ORG/$image@$imageRef\"|" ${REPO_DIR}/config/values.yml
+}
+
+buildAndReplaceImage metric-proxy ${REPO_DIR} Dockerfile metric_proxy
