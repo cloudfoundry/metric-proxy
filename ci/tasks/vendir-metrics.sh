6b5fdee2f47a2a341a6fc3c3ae6817697f6e26e7
Caitlyn Yu <cyu@pivotal.io>
2020-07-13 11:44:55 -0600

    Fix the reference dir for real this time

--- ci/tasks/vendir-metrics.sh
+++ ci/tasks/vendir-metrics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-METRIC_PROXY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
+METRIC_PROXY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd ../.. && pwd )"
 
 pushd cf-for-k8s
   vendir sync -d config/_ytt_lib/github.com/cloudfoundry/metric-proxy="${METRIC_PROXY_DIR}"
