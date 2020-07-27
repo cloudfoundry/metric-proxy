2c7990a99579c7b1977ea5a2856575fc2acfca36
Louie Brann <lbrann@vmware.com>
2020-07-27 14:21:06 -0600

    update vendir scripts to new cf-for-k8s location
    Signed-off-by: Caitlyn Yu <caitlyny@vmware.com>

--- ci/tasks/vendir-metrics.sh
+++ ci/tasks/vendir-metrics.sh
@@ -4,5 +4,5 @@ set -ex
 METRIC_PROXY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd ../.. && pwd )"
 
 pushd cf-for-k8s
-  vendir sync -d config/_ytt_lib/github.com/cloudfoundry/metric-proxy="${METRIC_PROXY_DIR}"
+  vendir sync -d config/metrics/_ytt_lib/metric-proxy="${METRIC_PROXY_DIR}"
 popd
