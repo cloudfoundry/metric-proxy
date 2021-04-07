0d0e8a69a3798dbfefb15161431ee69f1fed2f88
Caitlyn Yu <caitlyny@vmware.com>
2021-04-07 12:55:46 -0600

    Refactor config values into standard format
    Co-authored-by: Andrew Wittrock <awittrock@vmware.com>
    
    [#177585471](https://www.pivotaltracker.com/story/show/177585471)

--- ci/tasks/update-values-and-vendir-metrics.sh
+++ ci/tasks/update-values-and-vendir-metrics.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # this is the CI version of updateConfigValues
 imageRef="$(cat metric-proxy-docker/repository)@$(cat metric-proxy-docker/digest)"
-sed -i'' -e "s| metric_proxy:.*| metric_proxy: \"$imageRef\"|" metric-proxy/config/values.yml
+sed -i'' -e "s| metric_proxy:.*| metric_proxy: \"$imageRef\"|" metric-proxy/config/values/images.yml
 
 # this is the CI version of hack/bump-cf-for-k8s.sh
 pushd cf-for-k8s
